ddf7232740e9c28ea0fbe283e344dd495c75ead2
khushbu2002 <khushbukoul2@gmail.com>
2026-06-05 16:42:32 +0530

    Added built-in locator examples

--- tests/LocatingMultipleElements.spec.js
+++ tests/LocatingMultipleElements.spec.js
@@ -11,7 +11,8 @@ test('LocateMultipleElements', async ({page})=> {
     //to locate multiple elements we will use  page.$$('a')
     
     //here we are using an anchor tag 'a' because it is matching with every element, but we must use 
-    const links = await page.$$('a'); //-> we got all the links
+   //  const links = await page.$$('a'); 
+   //-> we got all the links
 
     //now since we have like all the elements via anchor tag, as we know when we get the elements of the web page via querySelectorAll() it returns us a nodeList on which we perform the actions like forEach() etc
 
@@ -19,15 +20,30 @@ test('LocateMultipleElements', async ({page})=> {
 
     // now because we want to check or traverse or validate each element in the array, we are going to use the loops. we may even use HOI of array accodring to the requirements.
 
-     for(const link of links) {
-        //now what are we doing here, we are going to each and every element and extracting the text element from it
+   //   for(const link of links) {
+   //      //now what are we doing here, we are going to each and every element and extracting the text element from it
 
-      //   const linkText = await link.textContent();
-      //   console.log(linkText);
-      const linkText = await link.textContent();
-      console.log(`[${linkText}]`);
-      
+   //    //   const linkText = await link.textContent();
+   //    //   console.log(linkText);
+   //    const linkText = await link.textContent();
+   //    console.log(`[${linkText}]`);
+   //   }
 
-     }
+   //  await page.waitForSelector("//body[1]/div[5]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/h4[1]");
+
+
+   // const products = await page.$$("//body[1]/div[5]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/h4[1]")
+   const products = await page.$$("//h4[@class='card-title']");
+
+   // for ( const product of products)  {
+   //    // console.log(product,"PRODUCT------------------------------------------------------------------------------------------------")
+   //    const productName = await product.textContent();
+   //    console.log(productName)
+   // }
+
+   for (const product of products) {
+    const productName = await product.textContent();
+    console.log(productName);
+}
 
 }) // -> our good old test function
---
+++ tests/Locators_builtin.spec.js
@@ -0,0 +1,34 @@
+
+
+//  page.getByAltText() - to locate an element, usually image, by its text alternative.
+//  page.getByPlaceholder() - to locate an imput by placeholder
+//  page.getByRole() - to locate by explicit and implicit accessibility attributes.
+//  page.getByText - to locate by text content.
+//  page.getByLabel() - to locate a form control by associated label's text.
+//  page.getByTitle() - to locate an element by its title attribute.
+//  page.getByTestId() - to locate an element based on its data-testedid attribute (other attributes can be con)
+
+
+const {test, expect} = require('@playwright/test')
+
+test('Built-inLocators', async({page}) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    const logo = await page.getByAltText('company-branding')
+
+    await expect(logo).toBeVisible();
+
+    const username = await page.getByPlaceholder('Username').fill("Admin")
+
+    const password = await page.getByPlaceholder('Password').fill("admin123")
+
+    await page.getByRole("button", { type: 'submit'}).click()
+
+    const displayName = await page.locator("//p[@class='oxd-userdropdown-name']").textContent()
+
+    await expect(await page.getByText(displayName)).toBeVisible()
+
+
+
+})
